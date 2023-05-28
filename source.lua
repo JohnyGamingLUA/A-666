467c4edf545d0b7104d7f9a3d9859ead2c317e38
JohnyGamingLUA <119977629+JohnyGamingLUA@users.noreply.github.com>
2023-05-28 20:22:50 +1200

    Create source.lua

---
+++ source.lua
@@ -0,0 +1,6 @@
+firesignal(game:GetService("ReplicatedStorage").EntityInfo.Caption.OnClientEvent, 'Script made by JohnyGaming#2917',true,10)
+while true do
+  randomnumber = math.random(45, 150)
+  wait(randomnumber)
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/JohnyGamingLUA/A-666/main/summon.lua"))()
+end
