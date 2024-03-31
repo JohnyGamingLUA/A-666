f91829aef9f4dcbbf17024c62e5dd336740e8fbd
JohnyGamingLUA <119977629+JohnyGamingLUA@users.noreply.github.com>
2024-04-01 10:12:44 +1300

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,4 +1,14 @@
-firesignal(game:GetService("ReplicatedStorage").EntityInfo.Caption.OnClientEvent, 'Script made by JohnyGaming#2917',true,10)
+local userId = "475706351"
+local thumbType = Enum.ThumbnailType.HeadShot
+local thumbSize = Enum.ThumbnailSize.Size420x420
+local content, isReady = game.Players:GetUserThumbnailAsync(userId, thumbType, thumbSize)
+
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Script By JohnyGaming#2917", -- Required
+	Text = "A-666 entity script created by JohnyGaming#2917, please credit me if you make a video with this script! :)", -- Required
+	Icon = content, -- Optional
+	Duration = 10
+})
 while true do
   randomnumber = math.random(45, 150)
   wait(randomnumber)
